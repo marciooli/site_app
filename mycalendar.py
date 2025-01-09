d0d07d10195ab16333c64b00bbd3325606b73b78
Marcio Oliveira <mf.oliveir@hotmail.com>
2025-01-09 16:41:51 -0300

    commit9_1_6

--- mycalendar.py
+++ mycalendar.py
@@ -29,8 +29,8 @@ class calendar():
         creds.refresh(Request())
       else:
     
-        flow = InstalledAppFlow(
-           client_config=st.secrets.credentials.installed
+        flow = InstalledAppFlow.from_client_secrets_file(
+           client_config=st.secrets.credentials, scopes=SCOPES
         )
         creds = flow.run_local_server(port=0)
       # Save the credentials for the next run
